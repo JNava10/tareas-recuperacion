40ae41d08da0a3901970df2f87c776922ac758c1
Nava <juannr2002@gmail.com>
2024-03-18 14:49:17 +0100

    Lado de servidor del login terminado.

--- server/back-tareas/app/Http/Controllers/AuthController.php
+++ server/back-tareas/app/Http/Controllers/AuthController.php
@@ -3,17 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\helpers\Common;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response as SymphonyResponse;
 
 class AuthController extends Controller
 {
     function login(Request $request) {
-        //        $validate = $request->validate([
-        //            'email' => 'required|email|max:255',
-        //            'password' => 'required|max:255|string',
-        //        ]);
+        $validate = Validator::make(
+            $request->all(),
+            [
+                'email' => 'required|email|max:255',
+                'password' => 'required|max:255|string',
+            ]
+        );
+
+        if ($validate->fails()) return Common::sendStdResponse(
+            "Revisa la estructura de la petición e intentalo de nuevo.",
+            [
+                "logged" => false,
+            ],
+            SymphonyResponse::HTTP_BAD_REQUEST
+        );
 
         $authAttempt = Auth::attempt([
             'email' => $request->email,
@@ -28,8 +41,10 @@ class AuthController extends Controller
             SymphonyResponse::HTTP_UNAUTHORIZED
         );
 
+        $abilities = $this->getUserAbilities($request->email);
+
         $auth = Auth::user();
-        $token =  $auth->createToken($request->email)->plainTextToken;
+        $token =  $auth->createToken($request->email, $abilities)->plainTextToken;
 
         return Common::sendStdResponse(
             "Se ha iniciado sesión correctamente.",
@@ -38,5 +53,14 @@ class AuthController extends Controller
                 "token" => $token
             ]
         );
+    }
+
+    private function getUserAbilities(string $email)
+    {
+        return User::where('email', $email)
+            ->first()
+            ->roles
+            ->pluck('name')
+            ->toArray();
     }
 }
