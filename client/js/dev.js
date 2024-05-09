95cb570e5aa2f05d6f1a666302280ceb9fb9e86b
Juan Navarrete <36804672+JNava10@users.noreply.github.com>
2024-05-09 20:41:59 +0200

    Tarjeta de las tareas asignadas migrada a Flowbite. Lista de tareas asignadas completada

--- client/js/dev.js
+++ client/js/dev.js
@@ -1,7 +1,7 @@
 import * as taskApi from "../common/api/task.api.js";
 import {createElementFromString, getUserId} from "../common/services/common.service.js";
 import {colors} from "../common/consts.js";
-import {getLargeBadge} from "../common/flowbite/badge.js";
+import {getUnassignedTaskCard} from "../common/elements/tasks/taskCard.js";
 
 const mainContainer = document.querySelector('.container');
 
@@ -29,32 +29,8 @@ const buildAssignedTasksPanel = (tasks) => {
 
     tasks.forEach(task => {
         const progressColor = calculateProgressColor(task.progress)
-        const taskCardHtml = `<div class="card" id="${task.id}">
-          <div class="card-content">
-            <p class="title">${task.name}</p>
-            <p class="subtitle">${task.description}</p>
-            
-            <div class="is-flex is-align-items-center">
-                <progress class="progress m-0 is-${progressColor} mr-2" value="${task.progress}" max="100"></progress>
-                <span class="tag m-0 is-${progressColor}">${task.progress}</span>
-            </div>
-            
-            <div class="mt-3 is-flex is-align-items-center is-justify-content-space-between">
-                <div class="buttons">
-                    <button class="button is-danger is-dark remove-task"><i class="fa-solid fa-trash"></i></button>
-                </div>
-                
-                ${getLargeBadge(task.difficulty.name, 'gray').html}
-            </div>
-            
-            <div class="mt-3 is-flex is-align-items-center is-justify-content-left">
-                <img class="w-10 h-10 rounded-full mr-2" src="${task.assigned_by.pic_url}" alt="Rounded avatar">
-                <span>${task.assigned_by.name} ${task.assigned_by.first_lastname} ${task.assigned_by.second_lastname}</span>
-            </div>
-          </div>
-        </div>`
+        const taskCard = getUnassignedTaskCard(task).element
 
-        const taskCard = createElementFromString(taskCardHtml);
         panel.append(taskCard);
     })
 };
